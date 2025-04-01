fc48ee5d73c0aed54f9e6dd0f57c7e9e5bf6a9c6
josemaia2002 <111988541+josemaia2002@users.noreply.github.com>
2025-04-01 14:27:19 -0300

    Update User.java

--- src/main/java/com/spring/backend/model/User.java
+++ src/main/java/com/spring/backend/model/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="user", schema = "api")
+@Table(name="user", schema = "public")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
